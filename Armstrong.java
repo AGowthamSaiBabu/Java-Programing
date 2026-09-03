2c5302d4183c4ca545522980260ba38fae5fd43b
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-09-03 22:19:04 +0530

    Armstrong

--- Armstrong.java
+++ Armstrong.java
@@ -22,20 +22,44 @@
 
 // 2. Using for loop
 
+// public class Armstrong {
+//     public static void main(String[] args) {
+//         int num =153;
+//         int origin = num;
+//         int result =0;
+
+//         for(;num!=0;num =num/10){
+//             int digit =num%10;
+//             result =result+digit*digit*digit;
+//         }
+//         if(result == origin){
+//             System.out.println(origin + " is an Armstrong number");
+//         }else{
+//             System.out.println(origin+" is not Armstrong number");
+//         }
+//     }
+// }
+
+// 3. Using a method
+
 public class Armstrong {
+public static boolean isArmstrong(int num){
+    int origin =num;
+    int result =0;
+    while(num !=0){
+        int digit =num%10;
+        result =result+digit*digit*digit;
+        num =num /10;
+    }
+    return origin ==result;
+}
     public static void main(String[] args) {
         int num =153;
-        int origin = num;
-        int result =0;
-
-        for(;num!=0;num =num/10){
-            int digit =num%10;
-            result =result+digit*digit*digit;
-        }
-        if(result == origin){
-            System.out.println(origin + " is an Armstrong number");
+        if(isArmstrong(num)){
+            System.out.println("is Armstron num");
         }else{
-            System.out.println(origin+" is not Armstrong number");
+            System.out.println("is not Armstron num");
         }
+
     }
 }
