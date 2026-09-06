36bcdca16ea55117e9b8891a2a1c9ba5bad5218e
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-09-06 18:30:53 +0530

    HarshadNumber

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -19,27 +19,47 @@
 
 // Using a method
 
-public class HarshadNumber {
+// public class HarshadNumber {
 
-    public static boolean isHarshadNumber(int number){
-        int origin  = number;
+//     public static boolean isHarshadNumber(int number){
+//         int origin  = number;
+//         int sum =0;
+
+//         while(number!=0){
+//             int digit =number%10;
+//             sum =sum+digit;
+//             number = number / 10;
+//         }
+//         return origin % sum==0;
+//     }
+//     public static void main(String[] args) {
+//         int number =18;
+
+//         if(isHarshadNumber(number)){
+//             System.out.println("is a HarshadNumber");
+//         }else{
+//             System.out.println("is not HarshadNumber");
+//         }
+
+//     }
+// }
+
+// using String
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int num =18;
         int sum =0;
 
-        while(number!=0){
-            int digit =number%10;
-            sum =sum+digit;
-            number = number / 10;
-        }
-        return origin % sum==0;
-    }
-    public static void main(String[] args) {
-        int number =18;
+        String str = String.valueOf(num);
 
-        if(isHarshadNumber(number)){
-            System.out.println("is a HarshadNumber");
+        for(int i=0;i<str.length();i++){
+            sum =sum+(str.charAt(i) -'0');
+        }
+        if(num % sum ==0){
+            System.out.println(num+"is a HarshadNumber");
         }else{
-            System.out.println("is not HarshadNumber");
+            System.out.println(num+"is not a HarshadNumber");
         }
-
     }
 }
