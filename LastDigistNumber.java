fa2c0b4c46e6e2b83f98619e75f854121a3c1b42
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-09-15 06:50:16 +0530

    LastDigistNumber

--- LastDigistNumber.java
+++ LastDigistNumber.java
@@ -16,19 +16,35 @@
 
 // Using String
 
+// public class  LastDigistNumber {
+//     public static void main(String[] args) {
+//         int number = 5729;
+//         String str = String.valueOf(number);
+//         int LastDigistNumber =0;
+
+//         for(int i =0;i<str.length();i++){
+//             int digit =str.charAt(i)-'0';
+
+//             if(digit>LastDigistNumber){
+//                 LastDigistNumber =digit;
+//             }
+//         }
+//         System.out.println("LastDigistNumber :"+LastDigistNumber);
+//     }
+// }
+
+// 2. Without String, using % 10 — your method
+
 public class  LastDigistNumber {
     public static void main(String[] args) {
         int number = 5729;
-        String str = String.valueOf(number);
         int LastDigistNumber =0;
 
-        for(int i =0;i<str.length();i++){
-            int digit =str.charAt(i)-'0';
-
-            if(digit>LastDigistNumber){
-                LastDigistNumber =digit;
-            }
+        while(number>0){
+            int digit = number%10;
+            LastDigistNumber = Math.max(LastDigistNumber,digit);
+            number =number/ 10;
         }
-        System.out.println("LastDigistNumber :"+LastDigistNumber);
+        System.out.println("LastDigistNumber : "+LastDigistNumber);
     }
 }
