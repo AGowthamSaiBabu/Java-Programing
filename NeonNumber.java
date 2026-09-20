af9840d6e17d10a5e2ef1a4fd91788e19663de7d
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-09-20 19:05:55 +0530

    NeonNumber

--- NeonNumber.java
+++ NeonNumber.java
@@ -1,17 +1,38 @@
+// public class NeonNumber{
+//     public static void main(String[] args) {
+//         int number =9;
+//         int square =number * number;
+//         int sum =0;
+
+//         while(square>0){
+//             sum +=square%10;
+//             square =square /10;
+//         }
+//         if(sum == number){
+//             System.out.println(number+"is s NeonNumber");
+//         }else{
+//             System.out.println(number+"is not NeonNumber");
+//         }
+//     }
+// }
+
+// 2 Using String
+
 public class NeonNumber{
     public static void main(String[] args) {
         int number =9;
-        int square =number * number;
+        int square =number*number;
         int sum =0;
 
-        while(square>0){
-            sum +=square%10;
-            square =square /10;
+        String str =String.valueOf(square);
+
+        for(int i=0;i<str.length();i++){
+            sum = sum+(str.charAt(i)-'0');
         }
-        if(sum == number){
-            System.out.println(number+"is s NeonNumber");
+        if(sum ==number){
+            System.out.println("NeonNumber");
         }else{
-            System.out.println(number+"is not NeonNumber");
+            System.out.println("Not a NeonNumber");
         }
     }
 }
